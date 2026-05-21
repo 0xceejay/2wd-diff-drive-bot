5f25b71bc4d6c78bc899699171ed4d9e8e94e745
0xceejay <0xceejay@gmail.com>
2026-05-21 22:18:34 +0100

    fix config

--- src/bot_bringup/launch/teleop.launch.py
+++ src/bot_bringup/launch/teleop.launch.py
@@ -7,7 +7,6 @@ import launch_ros.actions
 
 
 def generate_launch_description():
-    joy_config = launch.substitutions.LaunchConfiguration('joy_config')
     joy_dev = launch.substitutions.LaunchConfiguration('joy_dev')
     publish_stamped_twist = launch.substitutions.LaunchConfiguration(
         'publish_stamped_twist')
@@ -16,25 +15,26 @@ def generate_launch_description():
 
     return launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument('joy_vel', default_value='cmd_vel'),
-        launch.actions.DeclareLaunchArgument('joy_config', default_value='ps3'),
         launch.actions.DeclareLaunchArgument('joy_dev', default_value='0'),
         launch.actions.DeclareLaunchArgument(
             'publish_stamped_twist', default_value='false'
         ),
-        launch.actions.DeclareLaunchArgument('config_filepath', default_value=[
-            launch.substitutions.TextSubstitution(text=os.path.join(
-                get_package_share_directory('teleop_twist_joy'), 'config', '')),
-            joy_config,
-            launch.substitutions.TextSubstitution(text='.config.yaml')]),
+        launch.actions.DeclareLaunchArgument(
+            'config_filepath',
+            default_value=os.path.join(
+                get_package_share_directory('bot_bringup'),
+                'config',
+                'teleop.config.yaml'
+            )
+        ),
 
         launch_ros.actions.Node(
             package='joy', executable='joy_node', name='joy_node',
             parameters=[{
                 'device_id': joy_dev,
-                'deadzone': 0.3,
+                'deadzone': 0.1,
                 'autorepeat_rate': 20.0,
-            }, config_filepath
-            ]
+            }]
         ),
 
         launch_ros.actions.Node(
